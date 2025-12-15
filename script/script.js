08ad8feca965894f9a2ea188fc0c898b3cbc7e9a
Ygor Scofano <cinzeiro@outlook.com.br>
2025-12-15 13:05:19 -0300

    changes

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const text = "A Manhattan Pro Sales gera conversas qualificadas e leads prontos para o seu time comercial por meio de prospecção ativa via WhatsApp. Operação humana, segmentada e focada em empresários e decisores que realmente importam.";
 function writeText (text) {
     const element = document.querySelector('.banner_p');
+    element.textContent = "";
     let i = 0;
     const interval = setInterval(() => {
         if (i < text.length) {
@@ -12,3 +13,14 @@ function writeText (text) {
     }, 40);
 };
 writeText(text);
+const header = document.querySelector('.header');
+function stickyHeader(header) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    });
+};
+stickyHeader(header);
