de8e41d6e7b2da13878632f0023055cdef379792
Ygor Scofano <cinzeiro@outlook.com.br>
2026-02-22 17:29:00 -0300

    Cool footer gradient animation added to script.js

--- script/script.js
+++ script/script.js
@@ -1,26 +1,35 @@
 const text = "A Manhattan Pro Sales gera conversas qualificadas e leads prontos para o seu time comercial por meio de prospecção ativa via WhatsApp. Operação humana, segmentada e focada em empresários e decisores que realmente importam.";
-function writeText (text) {
-    const element = document.querySelector('.banner_p');
-    element.textContent = "";
-    let i = 0;
-    const interval = setInterval(() => {
-        if (i < text.length) {
-            element.textContent += text.charAt(i);
-            i++;
-        } else {
-            clearInterval(interval);
-        }
-    }, 40);
-};
+function writeText(text) {
+	const element = document.querySelector(".banner_p");
+	element.textContent = "";
+	let i = 0;
+	const interval = setInterval(() => {
+		if (i < text.length) {
+			element.textContent += text.charAt(i);
+			i++;
+		} else {
+			clearInterval(interval);
+		}
+	}, 40);
+}
 writeText(text);
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 function stickyHeader(header) {
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 0) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-    });
-};
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 0) {
+			header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
+	});
+}
 stickyHeader(header);
+function moveGradient() {
+	const footer = document.querySelector(".footer");
+	let angle = 0;
+	setInterval(() => {
+		angle = (angle + 1) % 360; // reinicia automaticamente
+		footer.style.background = `linear-gradient(${angle}deg, #131E2A 0%, #000000 100%)`;
+	}, 10);
+};
+moveGradient();
